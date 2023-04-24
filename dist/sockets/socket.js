4f9f6cfcbf2759697a4e7c6683575ae7244c43b3
pozito31 <jessicapozo198715@gmail.com>
2023-04-24 09:52:15 +0200

    Segundo comit

---
+++ dist/sockets/socket.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.mensaje = exports.desconectar = void 0;
+const desconectar = (cliente) => {
+    cliente.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+};
+exports.desconectar = desconectar;
+// Escuchar mensajes
+const mensaje = (cliente, io) => {
+    cliente.on('mensaje', (payload) => {
+        console.log('Mensaje recibido', payload);
+        io.emit('mensaje-nuevo', payload);
+    });
+};
+exports.mensaje = mensaje;
